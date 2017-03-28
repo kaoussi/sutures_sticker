426a994a49d886d35236650d3fd42aa3469f0146
kaoussi <kaoussi.souhail@gmail.com>
2017-03-28 01:11:02 +0100

    initial commit

---
+++ app/src/main/java/team/uptech/motionviews/ui/StickerSelectActivity.java
@@ -0,0 +1,159 @@
+package team.uptech.motionviews.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import team.uptech.motionviews.R;
+
+/**
+ * selects sticker
+ * result - Integer, resource id of the sticker, bundled at key EXTRA_STICKER_ID
+ * <p>
+ * Stickers borrowed from : http://www.flaticon.com/packs/pokemon-go
+ */
+
+public class StickerSelectActivity extends AppCompatActivity {
+
+    public static final String EXTRA_STICKER_ID = "extra_sticker_id";
+
+    private final int[] stickerIds = {
+            R.drawable.asset2,
+            R.drawable.asset3,
+            R.drawable.asset4,
+            R.drawable.asset5,
+            R.drawable.asset6,
+            R.drawable.asset7,
+            R.drawable.asset9,
+            R.drawable.asset10,
+            R.drawable.asset11,
+            R.drawable.asset12,
+            R.drawable.asset13,
+            R.drawable.asset14,
+            R.drawable.asset16,
+            R.drawable.asset17,
+            R.drawable.asset18,
+            R.drawable.asset19,
+            R.drawable.asset20,
+            R.drawable.asset21,
+            R.drawable.asset23,
+            R.drawable.asset24,
+            R.drawable.asset25,
+            R.drawable.asset26,
+            R.drawable.asset27,
+            R.drawable.asset28,
+            R.drawable.asset30,
+            R.drawable.asset31,
+            R.drawable.asset32,
+            R.drawable.asset33,
+            R.drawable.asset34,
+            R.drawable.asset35,
+            R.drawable.asset37,
+            R.drawable.asset38,
+            R.drawable.asset39,
+            R.drawable.asset40,
+            R.drawable.asset41,
+            R.drawable.asset42
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_sticker_activity);
+
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.stickers_recycler_view);
+        GridLayoutManager glm = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(glm);
+
+        List<Integer> stickers = new ArrayList<>(stickerIds.length);
+        for (Integer id : stickerIds) {
+            stickers.add(id);
+        }
+
+        recyclerView.setAdapter(new StickersAdapter(stickers, this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onStickerSelected(int stickerId) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_STICKER_ID, stickerId);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    class StickersAdapter extends RecyclerView.Adapter<StickersAdapter.StickerViewHolder> {
+
+        private final List<Integer> stickerIds;
+        private final Context context;
+        private final LayoutInflater layoutInflater;
+
+        StickersAdapter(@NonNull List<Integer> stickerIds, @NonNull Context context) {
+            this.stickerIds = stickerIds;
+            this.context = context;
+            this.layoutInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public StickerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new StickerViewHolder(layoutInflater.inflate(R.layout.sticker_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(StickerViewHolder holder, int position) {
+            holder.image.setImageDrawable(ContextCompat.getDrawable(context, getItem(position)));
+        }
+
+        @Override
+        public int getItemCount() {
+            return stickerIds.size();
+        }
+
+        private int getItem(int position) {
+            return stickerIds.get(position);
+        }
+
+        class StickerViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView image;
+
+            StickerViewHolder(View itemView) {
+                super(itemView);
+                image = (ImageView) itemView.findViewById(R.id.sticker_image);
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = getAdapterPosition();
+                        if (pos >= 0) { // might be NO_POSITION
+                            onStickerSelected(getItem(pos));
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
